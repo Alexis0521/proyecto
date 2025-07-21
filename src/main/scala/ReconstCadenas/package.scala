08c9ecbc76b0884c70eac41b016cc3e959baab24
Alexis0521 <128717747+Alexis0521@users.noreply.github.com>
2025-07-21 17:28:48 -0500

    Add files via upload

--- src/main/scala/ReconstCadenas/package.scala
+++ src/main/scala/ReconstCadenas/package.scala
@@ -4,20 +4,7 @@ import Oraculo.*
 import scala.annotation.tailrec
 
 package object ReconstCadenas {
-
-  def reconstruirCadenaIngenuo(n: Int, o: Oraculo.Oraculo): Seq[Char] = {
-    def generarTodasLasCadenas(n: Int): LazyList[Seq[Char]] = {
-      if (n == 0) LazyList(Seq())
-      else for {
-        prefijo <- generarTodasLasCadenas(n - 1)
-        letra <- Oraculo.alfabeto.to(LazyList)
-      } yield prefijo :+ letra
-    }
-
-    generarTodasLasCadenas(n).find(o).getOrElse(
-      throw new RuntimeException("No se encontró una cadena que el oráculo acepte.")
-    )
-  }
+  
 
   def reconstruirCadenaMejorado(n: Int, o: Oraculo.Oraculo): Seq[Char] = {
     val alfabeto = Oraculo.alfabeto
@@ -41,8 +28,6 @@ package object ReconstCadenas {
     construir(1, Set(Seq()))
   }
 
-
-
   def imprimirTrie(t: Trie, nivel: Int = 0): Unit = {
     val indent = "  " * nivel
     t match {
@@ -53,28 +38,6 @@ package object ReconstCadenas {
         println(s"$indent- Hoja('$c') marcada: $marcada")
     }
   }
-  def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
-    val arbolVacio = Nodo('_', false, List())
-
-    def generarCombinaciones(prefijo: Seq[Char], arbol: Trie, resultado:Trie) : Trie = {
-      // Se detiene si el oraculo determina que el prefijo no pertenece
-      if (!o(prefijo)) return resultado
-      // Si el prefijo no es vacio agrega el prefijo al Trie.
-      val nuevoArbol = if (prefijo.nonEmpty) adicionar(prefijo, arbol) else arbol
-
-      // Si se llega a una palabra completa (longitud n), se agrega al Trie de resultados
-      val resultadoActualizado =
-        if (prefijo.length == n) adicionar(prefijo, resultado)
-        else resultado
-      // Explora todos los posibles caracteres que siguen
-      val siguientes = alfabeto.filter(char => o(prefijo :+ char))
-
-      //Procesa cada caracter de forma recursiva
-      siguientes.foldLeft(resultadoActualizado) { (resAcumulado, char) =>
-        val nuevoPrefijo = prefijo :+ char
-        generarCombinaciones(nuevoPrefijo, nuevoArbol, resAcumulado)
-      }
-    }
 
 
     def extraerPalabras(trie: Trie): Seq[Seq[Char]] = {
@@ -100,38 +63,7 @@ package object ReconstCadenas {
       recorrer(trie, Seq.empty)
     }
 
-    val arbolResultado = generarCombinaciones(Seq.empty, arbolVacio, arbolVacio)
-    val palabras = extraerPalabras(arbolResultado)
-    /* Para pruebas solamente
-    imprimirTrie(arbolResultado)
 
-    
-    println(palabras)
-    println("Palabras extraídas:")
-    palabras.foreach(p => println(p.mkString))
-    
-     */
-    // Retorna la palabra encontrada
-    palabras.headOption.getOrElse(Seq.empty)
-  }
-  /*
-    def reconstruirCadenaTurboAcelerada(n: Int, o: Oraculo): Seq[Char] = {
-      val arbolVacio = Nodo('_', false, List())
-  
-      def generarCombinaciones(prefijo: Seq[Char], arbol: Trie) : Option[Seq[Char]] = {
-  
-        if (!o(prefijo)) return None
-  
-        val nuevoArbol = if (prefijo.nonEmpty) adicionar(prefijo, arbol) else arbol
-  
-  
-        val siguientes = alfabeto.filter(char => o(prefijo :+ char))
-  
-        alfabeto
-          .filter(char => o(prefijo :+ char))
-          .flatMap(char => generarCombinaciones(prefijo :+ char, nuevoArbol))
-          .headOption
-      }
-      generarCombinaciones(Seq.empty,arbolVacio).getOrElse(Seq.empty)
-    }*/
+
 }
+
--- src/main/scala/ReconstCadenasPar/PruebaArbolSufijos.scala
+++ src/main/scala/ReconstCadenasPar/PruebaArbolSufijos.scala
@@ -1,22 +1,6 @@
 object PruebaArbolSufijos extends App {
   import ArbolSufijos._
-  /*
-    // Secuencias de prueba
-    val secuencias = Seq("hola".toSeq, "sol".toSeq)
   
-    // Construimos el árbol de sufijos
-    val arbol = arbolDeSufijos(secuencias)
-  
-    // Pruebas de pertenencia
-    val pruebas = Seq("hola", "ola", "la", "a", "sol", "ol", "l", "no")
-  
-    println("Resultados de búsqueda:")
-    pruebas.foreach { palabra =>
-      val perteneceAlArbol = pertenece(palabra.toSeq, arbol)
-      println(f"$palabra%-5s -> ${if (perteneceAlArbol) "Sí" else "No"}")
-    }
-  */
-
   // Impresión estructurada (muy simple) del árbol
   def imprimirTrie(t: Trie, nivel: Int = 0): Unit = {
     val indent = "  " * nivel
@@ -28,10 +12,7 @@ object PruebaArbolSufijos extends App {
         println(s"$indent- Hoja('$c') marcada: $marcada")
     }
   }
-
-
-
-
+  
 
   val palabras = Seq("hola".toSeq, "sol".toSeq)
   val arbolPrueba = arbolDeSufijos(palabras)
